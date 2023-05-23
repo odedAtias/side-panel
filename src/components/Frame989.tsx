e5c9e096c8d3e95519d7b823597d0d2ddc5f5334
Oded <odedatias8115@gmail.com>
2023-05-23 19:01:24 +0300

    Adding Svg icons

--- src/components/Frame989.tsx
+++ src/components/Frame989.tsx
@@ -11,28 +11,51 @@ const Frame989 = ({total, fairValue, difference, variance}: Props) => {
 		<div className='f-991-item' style={{width: '100%'}}>
 			<div className='f-989'>
 				<div className='totalTitle'>
-					<p>icon</p>
+					{/* Icon  */}
+					<svg
+						width='20'
+						height='20'
+						viewBox='0 0 20 20'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<circle cx='10' cy='10' r='9.5' stroke='#568793' />
+						<path
+							d='M11.5625 6.35419H8.4375L10.8929 9.71957L8.4375 13.125H11.5625'
+							stroke='#568793'
+							stroke-linecap='round'
+							stroke-linejoin='round'
+						/>
+					</svg>
 					<p style={{color: '#8B9092', fontSize: '11px'}}>Total title</p>
 				</div>
 				<div className='summarize'>
 					{/* Total */}
 					<div className='f-989-item'>
-						<p className='val'>{total}</p>
+						<p className='val' style={{fontWeight: 'bold'}}>
+							${total}
+						</p>
 						<p className='key'>Total</p>
 					</div>
 					{/* Fair value */}
 					<div className='f-989-item'>
-						<p className='val'>{fairValue}</p>
+						<p className='val' style={{fontWeight: 'bold'}}>
+							${fairValue}
+						</p>
 						<p className='key'>Fair value</p>
 					</div>
 					{/* Difference */}
 					<div className='f-989-item'>
-						<p className='val'>{difference}</p>
+						<p className='val' style={{fontWeight: 'bold'}}>
+							${difference}
+						</p>
 						<p className='key'>Difference</p>
 					</div>
 					{/* Variance */}
 					<div className='f-989-item'>
-						<p className='val'>{variance}</p>
+						<p className='val' style={{fontWeight: 'bold'}}>
+							{variance}%
+						</p>
 						<p className='key'>Variance</p>
 					</div>
 				</div>
--- src/layout/Frame1018.tsx
+++ src/layout/Frame1018.tsx
@@ -5,6 +5,23 @@ import Frame1012 from '../components/Frame1012';
 const Frame1018 = () => {
 	return (
 		<div className='f-1018'>
+			<svg
+				width='32'
+				height='32'
+				viewBox='0 0 32 32'
+				fill='none'
+				xmlns='http://www.w3.org/2000/svg'
+			>
+				<circle cx='16' cy='16' r='15.5' stroke='#568793' />
+				<path
+					d='M14.7637 20L18.0364 16.4L14.7637 12.8'
+					stroke='#568793'
+					stroke-width='0.8'
+					stroke-linecap='round'
+					stroke-linejoin='round'
+				/>
+			</svg>
+
 			<Frame1012 />
 		</div>
 	);
